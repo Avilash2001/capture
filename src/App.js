480fc9bdd08861836906dd82b8bc5c066aa0c911
Avilash <avilashghosh2001@gmail.com>
2021-01-19 22:38:59 +0530

    9. REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -6,13 +6,32 @@ import Nav from './components/Nav';
 
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+
+//Router
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+
       <Nav/>
-      <AboutUs/>
+      <Switch>
+        <Route path = "/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path = "/work" exact>
+          <OurWork />
+        </Route>
+
+        <Route path = "/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
+      
     </div>
   );
 }

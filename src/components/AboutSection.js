7dc0e812eb0ba286478c47c23787c8d053bb8df9
Avilash <avilashghosh2001@gmail.com>
2021-01-20 11:37:36 +0530

    12. Added Some Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,44 @@
 import home1 from '../img/home1.png';
-//Import Styled
-// import styled from 'styled-components';
 import {Layout,Description, Image,Hide} from '../styles';
 
+//Framer Motion
+import { motion } from "framer-motion";
+
 const AboutSection = () => {
+
+    const titleAnim = {
+        hidden: { opacity: 0},
+        show: { opacity: 1,transition:{duration: 2}}
+    }
+
+    const containerAnim = {
+        hidden: {x: 100},
+        show: {x:0,transition:{duration: 1, ease: "easeOut" ,staggerChildren:1 }}
+    }
+    
     return(
         <Layout>
             <Description>
-                <div className="title">
+                <motion.div variants={containerAnim} initial="hidden" animate="show" className="title">
 
                     <Hide>
-                        <h2>We Work To Make</h2>
+                        <motion.h2 variants ={titleAnim}>
+                            We Work To Make
+                        </motion.h2>
                     </Hide>
 
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants ={titleAnim}>
+                            your <span>dreams</span> come
+                        </motion.h2>
                     </Hide>
 
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants ={titleAnim}>
+                            true.
+                        </motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
 
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
 

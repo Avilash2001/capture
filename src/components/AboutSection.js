360360fd3701853dcebfe81449f190a7ca6238ea
Avilash <avilashghosh2001@gmail.com>
2021-01-20 13:27:56 +0530

    14. About Us Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,8 @@ import {Layout,Description, Image,Hide} from '../styles';
 
 //Framer Motion
 import { motion } from "framer-motion";
+//Animations
+import { titleAnim,fade,photoAnim } from "../animation";
 
 const AboutSection = () => {
 
@@ -13,31 +15,33 @@ const AboutSection = () => {
                 <motion.div className="title">
 
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             We Work To Make
                         </motion.h2>
                     </Hide>
 
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come
                         </motion.h2>
                     </Hide>
 
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             true.
                         </motion.h2>
                     </Hide>
                 </motion.div>
 
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
+                <motion.p variants={fade}>
+                    Contact us for any photography or videography ideas that you have. We have professionals with amazing skills
+                </motion.p>
 
-                <button>Conatct Us</button>
+                <motion.button variants={fade}>Conatct Us</motion.button>
             </Description>
 
             <Image>
-                <img src={home1} alt="Guy with a camera"/>
+                <motion.img variants={photoAnim} src={home1} alt="Guy with a camera"/>
             </Image>
         </Layout>
     )
